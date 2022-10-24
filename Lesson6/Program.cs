c668acbe974c695cfddb0ae11d8cf89f83ceebfa
KDS060606 <114130994+KDS060606@users.noreply.github.com>
2022-10-24 15:00:47 +0500

    Update Program.cs

--- Lesson6/Program.cs
+++ Lesson6/Program.cs
@@ -26,17 +26,17 @@
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-Console.WriteLine("Введите значения для формулы линии: y = k1 * x + b1 ");
+
 Console.Write("k1 =  ");
 double k1 = double.Parse(Console.ReadLine());
 Console.Write("b1 =  ");
 double b1 = double.Parse(Console.ReadLine());
-Console.WriteLine("Введите y = k2 * x + b2");
+
 Console.Write("k2 =  ");
 double k2 = double.Parse(Console.ReadLine());
 Console.Write("b2 =  ");
 double b2 = double.Parse(Console.ReadLine());
-void DrawIntersection (double k1, double b1, double k2, double b2) // если координаты пересечения дробные, то не отмечает точку пересечения =(
+void DrawIntersection (double k1, double b1, double k2, double b2) 
 {
 k1 = (int)k1; b1 = (int)b1; k2 = (int)k2; b2 = (int)b2;
 for (int i = -7; i < 7; i++)
